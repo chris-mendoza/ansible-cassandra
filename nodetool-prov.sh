0932938cebd611dedb94d2512b5c56d4af138b8c
chris-mendoza <ccmendoza89@gmail.com>
2015-02-23 19:12:12 -0600

    adding nodetool playbook and gitignore

---
+++ nodetool-prov.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#This is the master provisioning script
+
+
+#provision nodes
+ansible-playbook -i scripts/rax.py config/nodetool-server.yml -f 20
+
+#configure nodes
+ansible-playbook -i scripts/rax.py config/nodetool.yml -f 20
+
+#ping them
+ansible -vvvv -u root -i scripts/rax.py db -m ping -f 20
