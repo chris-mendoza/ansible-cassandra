4cf5f198e19600ed592257704d916a59ef9611d1
chris-mendoza <ccmendoza89@gmail.com>
2015-02-23 19:21:47 -0600

    fixed nodetool group

--- nodetool-prov.sh
+++ nodetool-prov.sh
@@ -9,4 +9,4 @@ ansible-playbook -i scripts/rax.py config/nodetool-server.yml -f 20
 ansible-playbook -i scripts/rax.py config/nodetool.yml -f 20
 
 #ping them
-ansible -vvvv -u root -i scripts/rax.py db -m ping -f 20
+ansible -vvvv -u root -i scripts/rax.py nodetool -m ping -f 20
